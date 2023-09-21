0d26803a46aec413d6a3aef7139a9262c1829358
qzq <472597709@qq.com>
2023-09-21 10:55:39 +0800

    完善飞机边缘检测及初始位置

--- aircraft_battle/game.py
+++ aircraft_battle/game.py
@@ -1,15 +1,93 @@
 import pygame
 
 
+class Location(object):
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Aircraft(object):
+
+    def __init__(self, screen: pygame.Surface):
+        self.screen = screen
+
+        self.location = None  # 飞机位置
+        self.aircraft_image = None  # 飞机贴图
+
+        self.init()
+
+    def init(self):
+        self.init_aircraft()
+        self.init_location()
+
+    def init_location(self):
+        """
+        初始化飞机位置，屏幕最下方中间
+        :return:
+        """
+        scree_center_x, scree_center_y = self.screen.get_rect().center
+
+        x = scree_center_x - (self.aircraft_image.get_width() / 2)
+        y = self.screen.get_height() - self.aircraft_image.get_height()
+        self.location = Location(x, y)
+
+    def init_aircraft(self, path=None):
+        """
+        初始化飞机贴图，
+        :param path: 飞机贴图位置
+        :return:
+        """
+        if not path:
+            aircraft_image = pygame.image.load("./image/aircraft.png").convert_alpha()
+        else:
+            aircraft_image = pygame.image.load(path).convert_alpha()
+
+        self.aircraft_image = pygame.transform.scale(aircraft_image, (50, 50))
+
+    def update(self, key):
+        """
+        判断移动方向
+        :param key:
+        :return:
+        """
+
+        if key == pygame.K_RIGHT or key == pygame.K_d:
+            self.location.x += 10
+        elif key == pygame.K_LEFT or key == pygame.K_a:
+            self.location.x -= 10
+        elif key == pygame.K_UP or key == pygame.K_w:
+            self.location.y -= 10
+        elif key == pygame.K_DOWN or key == pygame.K_s:
+            self.location.y += 10
+
+        if self.location.x > self.screen.get_width() - self.aircraft_image.get_width():
+            self.location.x = self.screen.get_width() - self.aircraft_image.get_width()
+
+        if self.location.x < 0:
+            self.location.x = 0
+
+        if self.location.y > self.screen.get_height() - self.aircraft_image.get_height():
+            self.location.y = self.screen.get_height() - self.aircraft_image.get_height()
+
+        if self.location.y < 0:
+            self.location.y = 0
+
+        return
+
+    def draw(self):
+        self.screen.blit(self.aircraft_image, (self.location.x, self.location.y))
+
+
 def main():
     pygame.init()
 
     fps_clock = pygame.time.Clock()
-    screen = pygame.display.set_mode((800, 600))
-    pygame.key.set_repeat(100, 150)
+    screen = pygame.display.set_mode((600, 800))
+    pygame.key.set_repeat(100, 100)
 
-    aircraft = pygame.image.load("./image/aircraft.png").convert_alpha()
-    aircraft = pygame.transform.scale(aircraft, (100, 100))
+    aircraft = Aircraft(screen)
 
     done = False
 
@@ -22,21 +100,15 @@ def main():
             if event.type == pygame.QUIT:
                 done = True
             elif event.type == pygame.KEYDOWN:
-
-                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                    x += 20
-                elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                    x -= 20
-                elif event.key == pygame.K_UP or event.key == pygame.K_w:
-                    y -= 20
-                elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
-                    y += 20
+                if event.key in [pygame.K_RIGHT, pygame.K_LEFT, pygame.K_UP, pygame.K_DOWN,
+                                 pygame.K_a, pygame.K_w, pygame.K_s, pygame.K_d]:
+                    aircraft.update(event.key)
 
         screen.fill((255, 255, 255))
-        screen.blit(aircraft, (x, y))
+        aircraft.draw()
 
         pygame.display.update()
-        fps_clock.tick(10)
+        fps_clock.tick(30)
 
     pygame.quit()
 
